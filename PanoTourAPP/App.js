d3e603eb3b75a2e69d93f4b65cdf00c3d48e57c2
D <74151412+dv18uj@users.noreply.github.com>
2023-02-02 14:45:18 -0500

    pano implementation

--- PanoTourAPP/App.js
+++ PanoTourAPP/App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as THREE from 'three'
+import React, { Suspense, useRef } from 'react'
+import { Canvas, extend, useFrame, useThree, useLoader } from '@react-three/fiber'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import './style.css'
+
+extend({ OrbitControls })
+
+function Controls(props) {
+  const { camera, gl } = useThree()
+  const ref = useRef()
+  useFrame(() => ref.current.update())
+  return <orbitControls ref={ref} target={[0, 0, 0]} {...props} args={[camera, gl.domElement]} />
+}
+
+function Dome() {
+
+  const texture = useLoader(THREE.TextureLoader, require('./assets/office.jpg'))
+  return (
+    <mesh>
+      <sphereBufferGeometry attach="geometry" args={[500, 60, 40]} />
+      <meshBasicMaterial attach="material" map={texture} side={THREE.BackSide} />
+    </mesh>
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <Canvas camera={{ position: [0, 0, 0.1] }}>
+      <Controls enableZoom={false} enablePan={false} enableDamping dampingFactor={0.2} autoRotate rotateSpeed={-0.5} />
+      <Suspense fallback={null}>
+        <Dome />
+      </Suspense>
+    </Canvas>
+  )
+  }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
