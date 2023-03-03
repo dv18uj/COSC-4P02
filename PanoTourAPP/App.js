914baed4f30d53ffbc398d86f7e04a835c27ad41
D <74151412+dv18uj@users.noreply.github.com>
2023-03-03 11:58:39 -0500

    Move Dome to Tour

--- PanoTourAPP/App.js
+++ PanoTourAPP/App.js
@@ -1,21 +1,5 @@
 
 import React, { Suspense, useRef } from 'react';
-import { Canvas, extend, useFrame, useThree, useLoader } from '@react-three/fiber';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-
-import Dome from './Dome'; 
-import Menu from './Menu';
-import Navigation from './Navigation';
-import StartingScreen from './pages/StartingScreen'
-
-extend({ OrbitControls })
-
-function Controls(props) {
-  const { camera, gl } = useThree()
-  const ref = useRef()
-  useFrame(() => ref.current.update())
-  return <orbitControls ref={ref} target={[0, 0, 0]} {...props} args={[camera, gl.domElement]} />
-}
 
 export default function App() {
   return (
@@ -25,6 +9,6 @@ export default function App() {
         <Dome />
       </Suspense>
     </Canvas>*/
-    <StartingScreen/>
+    <Tour/>
   )
   };
--- PanoTourAPP/pages/Tour.js
+++ PanoTourAPP/pages/Tour.js
@@ -1,1 +1,28 @@
 //This page combines components to allow the user to tour 
+import React, { Suspense, useRef } from 'react';
+import { Canvas, extend, useFrame, useThree, useLoader } from '@react-three/fiber';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+import Dome from './Dome'; 
+
+extend({ OrbitControls })
+
+function Controls(props) {
+  const { camera, gl } = useThree()
+  const ref = useRef()
+  useFrame(() => ref.current.update())
+  return <orbitControls ref={ref} target={[0, 0, 0]} {...props} args={[camera, gl.domElement]} />
+}
+
+function Tour () {
+    return(
+    <Canvas camera={{ position: [0, 0, 0.1] }}>
+    <Controls enableZoom={false} enablePan={false} enableDamping dampingFactor={0.2} autoRotate rotateSpeed={-0.5} />
+    <Suspense fallback={null}>
+        <Dome />
+    </Suspense>
+    </Canvas>
+    );
+}
+
+export default Tour;
