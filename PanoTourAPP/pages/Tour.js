d96bde7b72d974ae4d774c1c1e571494a2d488b8
D <74151412+dv18uj@users.noreply.github.com>
2023-03-09 16:46:59 -0500

    update

--- PanoTourAPP/pages/Tour.js
+++ PanoTourAPP/pages/Tour.js
@@ -1,5 +1,5 @@
 //This page combines components to allow the user to tour 
-import React, { Suspense, useRef } from 'react';
+import React, { Suspense, useRef, useState } from 'react';
 import { Canvas, extend, useFrame, useThree, useLoader } from '@react-three/fiber';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import Button from '../atoms/Button'
@@ -14,17 +14,50 @@ function Controls(props) {
   useFrame(() => ref.current.update())
   return <orbitControls ref={ref} target={[0, 0, 0]} {...props} args={[camera, gl.domElement]} />
 }
+
+function Hotspot(props){
+  const mesh = useRef();
+
+  const [hovered, setHover] = useState(false);
+
+  useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
+  return(
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={[1,1,1]}
+      onPointerOver={e => setHover(true)}
+      onPointerOut={e => setHover(false)}>
+      
+      <boxGeometry attach="geometry" args ={[1,1,1]}/>
+      <meshStandardMaterial
+        attach="material"
+        color={hovered? 'white' : 'grey'}
+      />
+    </mesh>
+      )
+
+
+}
+
 const geometry = new THREE.BoxGeometry()
 const material = new THREE.MeshBasicMaterial()
 const mesh = new THREE.Mesh(geometry,material)
 function Tour () {
     return(
-    <Canvas camera={{ position: [0, 0, 0.1] }}>
+    /*<Canvas camera={{ position: [0, 0, 0.1] }}>
     <Controls enableZoom={false} enablePan={false} enableDamping dampingFactor={0.2} />
-    <div object={mesh}>open</div>
+    <ambientLight />
+    <pointLight position={[10, 10, 10]} />
+    <Hotspot position={[0,0,0]}/>
     <Suspense fallback={null}>
         <Dome />
     </Suspense>
+    </Canvas>*/
+    <Canvas camera={{ position: [0, 0, 0.1] }}>
+    <ambientLight />
+    <pointLight position={[10, 10, 10]} />
+    <Hotspot position={[0,0,0]}/>
     </Canvas>
     );
 }
