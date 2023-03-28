477fb4a604b3a5f33a57970f9905bebb66276a5a
WayneB17 <besseywayne@gmail.com>
2023-03-27 20:10:13 -0400

    needs work

--- PanoTourAPP/organisms/Dome.js
+++ PanoTourAPP/organisms/Dome.js
@@ -1,24 +1,28 @@
 import * as THREE from "three";
-import { useLoader } from "@react-three/fiber";
+import { useLoader, useFrame} from "@react-three/fiber";
 import NavMenu from "../templates/InfoPanel.jsx";
 import {Html} from "@react-three/drei";
-import React, {useState} from "react";
+import React, {useState, useEffect, useRef} from "react";
 import '../templates/infoPanel.css';
-
-function Dome({position, id}) {
+import img from '../assets/Vector.png';
+import { Camera } from "three";
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+function Dome({position, id, rotation}) {
     const [openPanel, setOpenPanel] = useState(false);
     const texture = useLoader(THREE.TextureLoader, require('../assets/office.jpg'))
-    return (
+    const icon_idle = useLoader(THREE.TextureLoader, img);
+ 
+      return (
       
       <group>
-      <mesh onClick={(e) => console.log(e.point)} /*Go to console on web developer tools to see coords. */ > 
+      <mesh > 
        <sphereBufferGeometry attach="geometry" args={[500, 60, 40]} />
        <meshBasicMaterial attach="material" map={texture} side={THREE.BackSide} />
       </mesh>
-    
-      <mesh position={position} id = {id} onClick={() => setOpenPanel(true)} >
-      <sphereGeometry args={[1, 10, 10]} />
-      <meshBasicMaterial color="grey" />
+       
+      <mesh position={position} rotation = {rotation} id = {id} onClick={() => setOpenPanel(true)} >
+      <planeBufferGeometry attach="geometry" args={[1, 1]} />
+      <meshBasicMaterial attach="material" map={icon_idle} />
       <Html center>
         <div className="container">
       <NavMenu open = {openPanel} onClose={()=> setOpenPanel(false)}/>
@@ -29,8 +33,7 @@ function Dome({position, id}) {
    
      </group>
      
-      
-    )
+      )
   }
 
   export default Dome;
--- PanoTourAPP/pages/Tour.js
+++ PanoTourAPP/pages/Tour.js
@@ -1,5 +1,5 @@
 //This page combines components to allow the user to tour 
-import React, { Suspense, useRef, useState } from 'react';
+import React, { Suspense, useEffect, useRef, useState } from 'react';
 import { Canvas, extend, useFrame, useThree, useLoader } from '@react-three/fiber';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import * as THREE from "three";
@@ -9,37 +9,47 @@ import InfoPanel from "../templates/InfoPanel.jsx";
 import SideMenu from "../organisms/NavMenu";
 import './tour.css';
 import '../templates/infoPanel.css';
-import {Html} from "@react-three/drei";
-import { Scene } from 'three';
+import { Vector3 } from 'three';
 
 const store = [
-  {position: [10, 4, -15], id: 0}, //top of door
-  {position: [7,-9, 10], id: 1} //laptop on table
+  {position: [10, 4, -15], rotation: [0,-0.5,0], id: 0}, //top of door
+  {position: [4, -6, 6], rotation: [0,-2.6,0], id: 1}, //laptop on table
+  {position: [-1, -4, 15], rotation: [0,-3,0], id: 2} //silver monitor with screen off
 ];
 
-  function ClickableObject({set}) {
+
+
+function ClickableObject({set}) {
   const {id, ...props} = store[set];
   return <Dome onClick = {() => setOpenPanel(true)} {...props} />
 }
 
-
 extend({ OrbitControls })
 function Controls(props) {
   const { camera, gl } = useThree()
-  const ref = useRef()
-  useFrame(() => ref.current.update())
-  return <orbitControls ref={ref} target={[0, 0, 0]} {...props} args={[camera, gl.domElement]} />
+  const ref = useRef();
+  useFrame((state) => {
+    if(Boolean(ref.current)){
+      camera.rotation.order = 'YXZ';
+    const {x,y,z} = state.camera.rotation;
+    //console.log([x,y,z]) //for finding rotation values
+    ref.current.update()
+    }
+  })
+  return <orbitControls ref={ref} target={[0, 0, 0]} {...props} args={[camera, gl.domElement]} /> 
 }
 
+
 function Tour () {
+
     return(
       <><SideMenu/>
       <Canvas  camera={{ position: [0, 0, 0.1] }}>
-        <Controls enableZoom={false} enablePan={false} enableDamping dampingFactor={0.2} />
+        <Controls enableZoom={false} enablePan={false} enableDamping dampingFactor={0.2}  />
         <Suspense fallback={null}>
-        
-        <ClickableObject set={0}/>
-        <ClickableObject set={1}/>
+        <ClickableObject set={0} />
+        <ClickableObject set={1} />
+        <ClickableObject set={2} />
         </Suspense>
       </Canvas> </>
     );
