6b1b938b87f0055547acd80ebbdb37635f2241f6
D <74151412+dv18uj@users.noreply.github.com>
2023-03-30 15:05:44 -0400

    V1.2

--- PanoTourAPP/atoms/Hotspot.js
+++ PanoTourAPP/atoms/Hotspot.js
@@ -1,15 +1,34 @@
 import { useNavigation } from '@react-navigation/native';
 import { useLoader} from "@react-three/fiber";
+import React, {useState, useEffect} from "react";
 import * as THREE from "three";
-import img from '../assets/Hotspot.png';
+import hover from '../assets/Hotspot_hover.png';
+import idle from '../assets/Hotspot.png';
 
 function Hotspot ({position, id, rotation}) {
     const navigation = useNavigation();
-    const icon_idle = useLoader(THREE.TextureLoader, img);
+    const icon_hover = useLoader(THREE.TextureLoader,hover)
+    const icon_idle = useLoader(THREE.TextureLoader, idle);
+    const [hovered, setHover] = useState(false);
+
+    useEffect(() => {
+      document.body.style.cursor = hovered ? 'pointer' : 'auto'
+    }, [hovered])
+
     return (
-        <mesh position={position} rotation = {rotation} id = {id} onClick={() => navigation.navigate('InfoPanel')} >
+        <mesh 
+            position={position} 
+            rotation = {rotation} 
+            id = {id} onClick={() => navigation.navigate('InfoPanel')}
+            onPointerOver = {(event) => setHover(true)}
+            onPointerOut = {(event) => setHover(false)}
+        >
             <planeBufferGeometry attach="geometry" args={[1, 1]}  />
-            <meshBasicMaterial attach="material" map={icon_idle} transparent = {true} />
+            <meshBasicMaterial 
+                attach="material"
+                map={hovered ? icon_idle : icon_hover}
+                transparent = {true} 
+            />
         </mesh>
     )
 }
