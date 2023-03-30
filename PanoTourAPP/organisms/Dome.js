4fbe818ef0ec1f927f9b1228b34b5423cf9c089e
WayneB17 <besseywayne@gmail.com>
2023-03-30 14:42:14 -0400

    added hover

--- PanoTourAPP/organisms/Dome.js
+++ PanoTourAPP/organisms/Dome.js
@@ -1,31 +1,45 @@
 import * as THREE from "three";
 import { useLoader, useFrame} from "@react-three/fiber";
-import NavMenu from "../templates/InfoPanel.jsx";
+import InfoPanel from "../templates/InfoPanel.jsx";
 import {Html} from "@react-three/drei";
 import React, {useState, useEffect, useRef} from "react";
-import '../templates/infoPanel.css';
-import img from '../assets/Vector.png';
+import idle from '../assets/Vector.png';
+import hover from '../assets/Vector_hover.png';
 import { Camera } from "three";
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+
 function Dome({position, id, rotation}) {
     const [openPanel, setOpenPanel] = useState(false);
     const texture = useLoader(THREE.TextureLoader, require('../assets/office.jpg'))
-    const icon_idle = useLoader(THREE.TextureLoader, img);
- 
+    const icon_idle = useLoader(THREE.TextureLoader, idle);
+    const icon_hover = useLoader(THREE.TextureLoader, hover);
+    const [hovered, setHover] = useState(false);
+    useEffect(() => {
+      document.body.style.cursor = hovered ? 'pointer' : 'auto'
+    }, [hovered])
+
+    
       return (
       
       <group>
-      <mesh > 
+      <mesh  > 
        <sphereBufferGeometry attach="geometry" args={[500, 60, 40]} />
        <meshBasicMaterial attach="material" map={texture} side={THREE.BackSide} />
       </mesh>
        
-      <mesh position={position} rotation = {rotation} id = {id} onClick={() => setOpenPanel(true)} >
+      <mesh 
+      position = {position} 
+      rotation = {rotation} 
+      id = {id} 
+      onClick={() => setOpenPanel(true)} 
+      onPointerOver = {(event) => setHover(true)}
+      onPointerOut = {(event) => setHover(false)} >
       <planeBufferGeometry attach="geometry" args={[1, 1]}  />
-      <meshBasicMaterial attach="material" map={icon_idle} transparent = {true} />
+      <meshBasicMaterial attach="material" map={hovered ? icon_idle : icon_hover} transparent = {true} />
       <Html center>
         <div className="container">
-      <NavMenu open = {openPanel} onClose={()=> setOpenPanel(false)}/>
+      <InfoPanel open = {openPanel} onClose={()=> setOpenPanel(false)}/>
       </div>
       </Html>
       
