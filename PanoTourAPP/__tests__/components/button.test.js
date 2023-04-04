f2a18be008d056e5aaf0dadfd8e5e35365e5980c
Mikes <35347523+akaJengo@users.noreply.github.com>
2023-04-04 15:13:27 -0400

    Config update

---
+++ PanoTourAPP/__tests__/components/button.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { fireEvent, render } from '@testing-library/react';
+import Button from '../../atoms/Button';
+
+describe('Button', () => {
+    test('renders correctly', () => {
+      const { getByText } = render(<Button title="Click me!" />);
+      const button = getByText('Click me!');
+      expect(button).toBeDefined();
+    });
+  
+    test('click event is triggered when button is clicked', () => {
+      const handleClick = jest.fn();
+      const { getByText } = render(<Button title="Click me!" onClick={handleClick} />);
+      const button = getByText('Click me!');
+      fireEvent.click(button);
+      expect(handleClick).toHaveBeenCalledTimes(1);
+    });
+  });
