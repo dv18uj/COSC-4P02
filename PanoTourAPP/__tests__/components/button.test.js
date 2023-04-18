70265a69a2ccb3087a39819f576422c6c92d9de1
Mikes <35347523+akaJengo@users.noreply.github.com>
2023-04-18 10:56:21 -0400

    testing
    Button and tour classes completed

--- PanoTourAPP/__tests__/components/button.test.js
+++ PanoTourAPP/__tests__/components/button.test.js
@@ -4,14 +4,14 @@ import Button from '../../atoms/Button';
 
 describe('Button', () => {
     test('renders correctly', () => {
-      const { getByText } = render(<Button title="Click me!" />);
+      const {getByText} = render(<Button title="Click me!" />);
       const button = getByText('Click me!');
       expect(button).toBeDefined();
     });
   
     test('click event is triggered when button is clicked', () => {
       const handleClick = jest.fn();
-      const { getByText } = render(<Button title="Click me!" onClick={handleClick} />);
+      const {getByText} = render(<Button title="Click me!" onClick={handleClick} />);
       const button = getByText('Click me!');
       fireEvent.click(button);
       expect(handleClick).toHaveBeenCalledTimes(1);
--- PanoTourAPP/__tests__/screens/tour.test.js
+++ PanoTourAPP/__tests__/screens/tour.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Tour from '../../pages/Tour';
 
+
 describe('Tour', () => {
   test('renders', () => {
     render(<Tour />);
