0aa6d80ed8cf25a28dd3e878ee2106019ae67ea0
Mikes <35347523+akaJengo@users.noreply.github.com>
2023-04-25 15:28:10 -0400

    Test Update
    This commit provided comments to the test classes as well as cleaning up unused files with no code.

--- PanoTourAPP/__tests__/screens/tour.test.js
+++ PanoTourAPP/__tests__/screens/tour.test.js
@@ -2,7 +2,12 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Tour from '../../pages/Tour';
 
-
+/** 
+ * This class tests if the Tour element will render. 
+ * It scans the localhost document for a element 
+ * called Panorama Tour, if this is the case
+ * the Tour element has succesfully rendered. 
+*/
 describe('Tour', () => {
   test('renders', () => {
     render(<Tour />);
