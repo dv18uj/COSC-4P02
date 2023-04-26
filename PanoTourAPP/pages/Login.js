b0e35566ad381d708b87f24c7a299620de203a7b
Mikes <35347523+akaJengo@users.noreply.github.com>
2023-04-25 20:21:14 -0400

    Login page test

--- PanoTourAPP/pages/Login.js
+++ PanoTourAPP/pages/Login.js
@@ -1,8 +1,73 @@
-import './Login.css'
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, Image, StyleSheet } from 'react-native';
+import background from "../assets/museum_background.jpg"
+import {useNavigation, useRoute} from '@react-navigation/native';
 
-function Login () {
-    return(
-        <div>
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const navigation = useNavigation();
+  const route = useRoute();
+
+  const handleLogin = () => {
+    // Handle login logic here
+  };
+
+  return (
+    <View style={styles.container}>
+        {/* <Image source={url({background})} style={styles.logo} /> */}
+      <TextInput
+        style={styles.input}
+        placeholder="Username"
+        onChangeText={setUsername}
+        value={username}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        onChangeText={setPassword}
+        value={password}
+        secureTextEntry={true}
+      />
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logo: {
+    width: 150,
+    height: 150,
+    marginBottom: 50,
+  },
+  input: {
+    width: '80%',
+    height: 50,
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: 'blue',
+    borderRadius: 5,
+    padding: 10,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 20,
+  },
+});
+
+export default Login;
 
         </div>
     );
