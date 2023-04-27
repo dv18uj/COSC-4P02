28caf73cca2091b79c1a53c43707dafbc321a825
zacole123 <ha18lj@brocku.ca>
2023-04-26 22:08:53 -0400

    Finished Info panel

--- PanoTourAPP/templates/InfoPanel.jsx
+++ PanoTourAPP/templates/InfoPanel.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import './infoPanel.css';
 import pic from '../assets/image1.PNG';
+import pic1 from '../assets/mag.PNG';
+import Tour from '../pages/Tour.js';
 
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import IconButton from "@mui/material/IconButton";
 import { Grid,Box } from '@mui/material';
+import { useNavigation } from "@react-navigation/native";
+import { useState } from 'react';
 
 
 const handleClick = (e) => {
@@ -13,13 +17,13 @@ const handleClick = (e) => {
   };
 
 
-const Menu = () => {
 
+const Menu = () => {
   
 
 return(
     <div className="infoPanel">
-        
+        <button class = "close" onClick={() => useNavigation.navigate('../pages/Tour.js')}>&times;</button>
         <div className="left">
             
             <div id="pictureDiv">
@@ -40,25 +44,26 @@ return(
             </div>
         </div>
         <div className="right">
-            
-            <h3>Building A/ Exhibit B </h3>
+        
+            <h3 class = "tag" id="top1">Building A/ Exhibit B </h3>
+            <div class="tag"><img  src={pic1} id="picture1"/><h1> Button</h1></div>
 
+            <p class="tag" id="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure  </p> 
+            
             <h4 id="title">
                 Title: aTitle
             </h4>
-
-            <p id="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure  </p> 
             
-            
-            <p id="date">
-                Date: 01/24/1998
-            </p>
+            <h4 id="date">
+                Date: 01/24/1997
+            </h4>
             
             {/* you can put a search bar over here */}
             
         </div>
     
     </div>
+    
 )
 
 }
